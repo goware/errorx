818c5d56ed089120e8a9c3e620e030436443f74f
Maciej Lisiewski <maciej.lisiewski@gmail.com>
2015-05-17 05:21:14 -0400

    make errorx fields public to fix JSON marshaling

--- errorx.go
+++ errorx.go
@@ -19,26 +19,26 @@ var verbosity uint8
 // Errorx is a more feature rich implementation of error interface inspired
 // by PostgreSQL error style guide
 type Errorx struct {
-	errorCode    int    `json:"error_code"`
-	errorMsg     string `json:"error_msg"`
-	errorDetails string `json:"error_details,omitempty"`
-	errorHint    string `json:"error_hint,omitempty"`
+	ErrorCode    int    `json:"error_code"`
+	ErrorMsg     string `json:"error_msg"`
+	ErrorDetails string `json:"error_details,omitempty"`
+	ErrorHint    string `json:"error_hint,omitempty"`
 }
 
 // New returns an error with error code and error messages provided in
 // function params
-func New(code int, errorMsg ...string) *Errorx {
-	e := Errorx{errorCode: code}
+func New(code int, ErrorMsg ...string) *Errorx {
+	e := Errorx{ErrorCode: code}
 
-	msgCount := len(errorMsg)
+	msgCount := len(ErrorMsg)
 	if msgCount > 0 {
-		e.errorMsg = errorMsg[0]
+		e.ErrorMsg = ErrorMsg[0]
 	}
 	if msgCount > 1 {
-		e.errorDetails = errorMsg[1]
+		e.ErrorDetails = ErrorMsg[1]
 	}
 	if msgCount > 2 {
-		e.errorHint = errorMsg[2]
+		e.ErrorHint = ErrorMsg[2]
 	}
 
 	return &e
@@ -55,59 +55,60 @@ func SetVerbosity(v uint8) {
 // Code returns Errorx error code value. It's intended primarily to allow
 // easy error comparison / matching
 func (e Errorx) Code() int {
-	return e.errorCode
+	return e.ErrorCode
 }
 
 // Error returns a string representation of errorx. It includes at least
 // error code and message. Error details and hint are provided depending
 // on verbosity level set
 func (e Errorx) Error() string {
-	if verbosity == 0 || (e.errorDetails == "" && e.errorHint == "") {
-		return fmt.Sprintf("error %d: %s", e.errorCode, e.errorMsg)
+	if verbosity == 0 || (e.ErrorDetails == "" && e.ErrorHint == "") {
+		return fmt.Sprintf("error %d: %s", e.ErrorCode, e.ErrorMsg)
 	}
-	if verbosity == 1 || e.errorHint == "" {
-		return fmt.Sprintf("error %d: %s - %s", e.errorCode, e.errorMsg, e.errorDetails)
+	if verbosity == 1 || e.ErrorHint == "" {
+		return fmt.Sprintf("error %d: %s - %s", e.ErrorCode, e.ErrorMsg, e.ErrorDetails)
 	}
-	return fmt.Sprintf("error %d: %s - %s - %s", e.errorCode, e.errorMsg, e.errorDetails, e.errorHint)
+	return fmt.Sprintf("error %d: %s - %s - %s", e.ErrorCode, e.ErrorMsg, e.ErrorDetails, e.ErrorHint)
 
 }
 
-// Errorf is a variant of Error that formats according to errorMsg
+// Errorf is a variant of Error that formats according to ErrorMsg
 // speficier and returns resulting string. Error details and hint
 // will not be formated
 func (e Errorx) Errorf(params ...interface{}) string {
-	if verbosity == 0 || (e.errorDetails == "" && e.errorHint == "") {
-		return fmt.Sprintf("error %d: %s", e.errorCode, fmt.Sprintf(e.errorMsg, params...))
+	if verbosity == 0 || (e.ErrorDetails == "" && e.ErrorHint == "") {
+		return fmt.Sprintf("error %d: %s", e.ErrorCode, fmt.Sprintf(e.ErrorMsg, params...))
 	}
-	if verbosity == 1 || e.errorHint == "" {
-		return fmt.Sprintf("error %d: %s - %s", e.errorCode, fmt.Sprintf(e.errorMsg, params...), e.errorDetails)
+	if verbosity == 1 || e.ErrorHint == "" {
+		return fmt.Sprintf("error %d: %s - %s", e.ErrorCode, fmt.Sprintf(e.ErrorMsg, params...), e.ErrorDetails)
 	}
-	return fmt.Sprintf("error %d: %s - %s - %s", e.errorCode, fmt.Sprintf(e.errorMsg, params...), e.errorDetails, e.errorHint)
+	return fmt.Sprintf("error %d: %s - %s - %s", e.ErrorCode, fmt.Sprintf(e.ErrorMsg, params...), e.ErrorDetails, e.ErrorHint)
 }
 
 // Json returns a json representation (as []byte) of errorx and error
 // if marshaling fails
 func (e Errorx) Json() ([]byte, error) {
-	err := Errorx{errorCode: e.errorCode, errorMsg: e.errorMsg}
-	if verbosity > 0 && e.errorDetails != "" {
-		err.errorDetails = e.errorDetails
+	err := Errorx{ErrorCode: e.ErrorCode, ErrorMsg: e.ErrorMsg}
+	if verbosity > 0 && e.ErrorDetails != "" {
+		err.ErrorDetails = e.ErrorDetails
 	}
-	if verbosity > 1 && e.errorHint != "" {
-		err.errorHint = e.errorHint
+	if verbosity > 1 && e.ErrorHint != "" {
+		err.ErrorHint = e.ErrorHint
 	}
-	return json.Marshal(err)
+	//spew.Dump(err)
+	return json.Marshal(&err)
 }
 
-// Jsonf is a variant of Json that formats according to errorMsg
+// Jsonf is a variant of Json that formats according to ErrorMsg
 // speficier and returns resulting string. Error details and hint
 // will not be formated
 func (e Errorx) Jsonf(params ...interface{}) ([]byte, error) {
-	err := Errorx{errorCode: e.errorCode, errorMsg: fmt.Sprintf(e.errorMsg, params...)}
-	if verbosity > 0 && e.errorDetails != "" {
-		err.errorDetails = e.errorDetails
+	err := Errorx{ErrorCode: e.ErrorCode, ErrorMsg: fmt.Sprintf(e.ErrorMsg, params...)}
+	if verbosity > 0 && e.ErrorDetails != "" {
+		err.ErrorDetails = e.ErrorDetails
 	}
-	if verbosity > 1 && e.errorHint != "" {
-		err.errorHint = e.errorHint
+	if verbosity > 1 && e.ErrorHint != "" {
+		err.ErrorHint = e.ErrorHint
 	}
 	return json.Marshal(err)
 }
